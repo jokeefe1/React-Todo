70ecb3f6aace49385f2cb67538e4412de09ae1af
John O'Keefe <jokeefe@weegolobal.com>
2019-03-07 13:24:22 -0500

    complete CRUD todo app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
 import React, { Component } from 'react';
-import { createNote, deleteNote } from './graphql/mutations';
+import { createNote, deleteNote, updateNote } from './graphql/mutations';
 import { listNotes } from './graphql/queries';
+import { onCreateNote } from './graphql/subscriptions'
 
 class App extends Component {
     state = {
+        id: '',
         note: '',
         notes: []
     };
@@ -15,6 +17,17 @@ class App extends Component {
         this.setState({ notes: result.data.listNotes.items });
     };
 
+    handleSetNote = ({ note, id }) => this.setState({ note, id });
+
+    hasExistingNote = () => {
+        const { id, notes } = this.state;
+        if (id) {
+            const isNote = notes.findIndex(note => note.id === id) > -1;
+            return isNote;
+        }
+        return false;
+    };
+
     handleChangeNote = e => {
         this.setState({ note: e.target.value });
     };
@@ -22,13 +35,33 @@ class App extends Component {
     handleAddNote = async e => {
         const { note, notes } = this.state;
         e.preventDefault();
-        const input = { note };
+        if (this.hasExistingNote()) {
+            this.handleUpdateNote();
+        } else {
+            const input = { note };
+            const result = await API.graphql(
+                graphqlOperation(createNote, { input })
+            );
+            const newNote = result.data.createNote;
+            const updatedNotes = [newNote, ...notes];
+            this.setState({ notes: updatedNotes, note: '' });
+        }
+    };
+
+    handleUpdateNote = async () => {
+        const { id, note, notes } = this.state;
+        const input = { id, note };
         const result = await API.graphql(
-            graphqlOperation(createNote, { input })
+            graphqlOperation(updateNote, { input })
         );
-        const newNote = result.data.createNote;
-        const updatedNotes = [newNote, ...notes];
-        this.setState({ notes: updatedNotes, note: '' });
+        const updatedNote = result.data.updateNote;
+        const index = notes.findIndex(note => note.id === id);
+        const updatedNotes = [
+            ...notes.slice(0, index),
+            updatedNote,
+            ...notes.slice(index + 1)
+        ];
+        this.setState({ notes: updatedNotes, note: '', id: '' });
     };
 
     handleDeleteNote = async noteId => {
@@ -43,10 +76,10 @@ class App extends Component {
     };
 
     render() {
-        const { note, notes } = this.state;
+        const { id, note, notes } = this.state;
         return (
             <div className="flex flex-column items-center justify-center pa3 bg-light-blue">
-                <h1 className="code f2-l">Amplify Notetaker</h1>
+                <h1 className="code f2-l">Amplify Todo</h1>
                 <form onSubmit={this.handleAddNote} className="mb3">
                     <input
                         type="text"
@@ -56,7 +89,7 @@ class App extends Component {
                         value={note}
                     />
                     <button type="submit" className="pa2 f4">
-                        Add Note
+                        { id ? 'Update' : 'Add'}
                     </button>
                 </form>
 
@@ -65,7 +98,12 @@ class App extends Component {
                     {notes.map(item => {
                         return (
                             <div key={item.id} className="flex items-center">
-                                <li className="list pa1 f3">{item.note}</li>
+                                <li
+                                    onClick={() => this.handleSetNote(item)}
+                                    className="list pa1 f3"
+                                >
+                                    {item.note}
+                                </li>
                                 <button
                                     onClick={() =>
                                         this.handleDeleteNote(item.id)
@@ -78,7 +116,6 @@ class App extends Component {
                         );
                     })}
                 </div>
-                {this.state.note}
             </div>
         );
     }
